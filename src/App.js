6351c9e82744a8e3cb9a0955663ddac3e7e4704f
clint360 <clint360.rebase@gmail.com>
2023-01-25 16:34:28 +0100

    Made some progress

--- src/App.js
+++ src/App.js
@@ -3,7 +3,17 @@ import {
   BrowserRouter as Router,
   Route,
 } from "react-router-dom";
-
+import Home from './pages/Home'
+import Q1 from './pages/Q1';
+import Q2 from './pages/Q2';
+import Q3 from './pages/Q3';
+import Q4 from './pages/Q4';
+import Q5 from './pages/Q5';
+import Q6 from './pages/Q6';
+import Q7 from './pages/Q7';
+import Q8 from './pages/Q8';
+import Q9 from './pages/Q9';
+import Q10 from './pages/Q10';
 
 function App() {
   return (
